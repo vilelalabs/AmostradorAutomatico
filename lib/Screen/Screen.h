a446eb031d6fd8afae862c342cf39fe698aef83b
hvilela0 <hvilela0@gmail.com>
2022-08-24 02:39:20 -0300

    Code organization

--- lib/Screen/Screen.h
+++ lib/Screen/Screen.h
@@ -1,26 +1,25 @@
 
 
-
-#include <TFTLabel.h>
 #include <TFTButton.h>
+#include <TFTLabel.h>
 
 #define MAX_LABELS 7
 #define MAX_BUTTONS 7
 
 #pragma once
 
-class Screen{
-    private:
-     TFTLabel labels[MAX_LABELS];
-     TFTButton buttons[MAX_BUTTONS];
+class Screen {
+   private:
+    TFTLabel labels[MAX_LABELS];
+    TFTButton buttons[MAX_BUTTONS];
 
-    public:
-        Screen();
+   public:
+    Screen();
 
-        Screen operator=(Screen &screen);
+    Screen operator=(Screen &screen);
 
-        void addLabel(unsigned short index, TFTLabel label);
-        void addButton(unsigned short index, TFTButton button);
-        void draw();
-        void readButtons();
+    void addLabel(unsigned short index, TFTLabel label);
+    void addButton(unsigned short index, TFTButton button);
+    void draw();
+    void readButtons();
 };
