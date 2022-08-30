2eb223fe9519059c5cd453f1e75b1df643ed4728
hvilela0 <hvilela0@gmail.com>
2022-08-30 15:47:41 -0300

    Final correction for MEGA (UNO leak)

--- lib/Screen/Screen.h
+++ lib/Screen/Screen.h
@@ -10,8 +10,8 @@
 #include <TFTButton.h>
 #include <TFTLabel.h>
 
-#define MAX_LABELS 4   // default minimum 8  / 4 for tests using Arduino Uno
-#define MAX_BUTTONS 4  // default minimum 7  / 4 for tests using Arduino Uno
+#define MAX_LABELS 8   // default minimum 8  / 4 for tests using Arduino Uno
+#define MAX_BUTTONS 8  // default minimum 7  / 4 for tests using Arduino Uno
 
 #pragma once
 
@@ -30,7 +30,7 @@ class Screen {
     void addLabel(uint8_t index, TFTLabel label);
     void addButton(uint8_t index, TFTButton button);
 
-    void changeLabel(uint8_t index, char *text, void (*callback)(TFTScreen *tft, Screen *tela));
+    void changeLabel(uint8_t index, char *text);
     void draw();
     int readButtons();
 
