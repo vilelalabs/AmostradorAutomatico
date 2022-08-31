30f57d61b3f1173290ec2cf4175f611788052140
hvilela0 <hvilela0@gmail.com>
2022-08-31 10:38:23 -0300

    added EEPROM and Temperature related functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <TimerOne.h>
+
 #include <main.h>
 
 unsigned long checkTime = millis();
@@ -8,8 +8,16 @@ void setup() {
     Serial.println("Starting...");
     tft.setup(DRIVER, ROTATION);
 
-     initTela(&tft, tela, TELA_TITULO);
-     delay(3000);
+    // garante que dados estão zerados na primeira vez que o programa é executado neste dispositivo
+    checkFirstRun();
+    // Garante início sempre do ciclo 1
+    EEPROMWriteInt(CICLO_INICIAL_EEPROM_ADDRESS, 1);
+
+    // inicializa o sensor de temperatura
+    setupTemperatureSensor();
+
+    // initTela(&tft, tela, TELA_TITULO);
+    // delay(4000);
     // initTela(&tft, tela, TELA_SELECAO_INICIAL);
     //   delay(5000);
     // initTela(&tft, tela, TELA_CONFIGURACAO);
@@ -20,18 +28,17 @@ void setup() {
     // delay(5000);
     // initTela(&tft, tela, TELA_CONFIG_CICLOS);
     // delay(5000);
-    //initTela(&tft, tela, TELA_INICIAR);
+    // initTela(&tft, tela, TELA_INICIAR);
     // delay(5000);
-    // initTela(&tft, tela, TELA_SELECIONAR_CICLO);
+     initTela(&tft, tela, TELA_SELECIONAR_CICLO);
     // delay(5000);
-     initTela(&tft, tela, TELA_EM_CICLO);
+    // initTela(&tft, tela, TELA_EM_CICLO);
     // delay(5000);
 
     // initTela(&tft, tela, TELA_TESTE);
 }
 
 void loop() {
-    
     if (millis() > checkTime + 250) {
         readButtonsTela(tela);
         checkTime = millis();
