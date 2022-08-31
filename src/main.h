74e44911ec25f84148777cb615c40f2189370f79
hvilela0 <hvilela0@gmail.com>
2022-08-31 10:37:48 -0300

    added headers for EEPROM and Temperature

--- src/main.h
+++ src/main.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <Arduino.h>
+#include <EEPROMFunctions.h>
 #include <Screen.h>
 #include <TFTButton.h>
 #include <TFTConfig.h>
 #include <TFTLabel.h>
 #include <TFTScreen.h>
 #include <Telas/Telas.h>
+#include <TemperatureSensor.h>
