92ba1f8e5611a30f17b2627f679654e283409574
hvilela0 <hvilela0@gmail.com>
2022-08-30 21:40:30 -0300

    changed 'temperatura' addr and added ciclo inicial

--- lib/EEPROMFunctions/EEPROMFunctions.h
+++ lib/EEPROMFunctions/EEPROMFunctions.h
@@ -6,15 +6,17 @@
 #define MAX_EEPROM_SPACE 1000
 
 // Mapeamento da EEPROM
-#define TEMPERATURA_EEPROM_ADDRESS      10
+
+#define CICLO_INICIAL_EEPROM_ADDRESS    0
 #define CICLO_LIMPEZA_EEPROM_ADDRESS    2
 #define CICLO_INSERCAO_EEPROM_ADDRESS   4
 #define CICLO_COLETA_EEPROM_ADDRESS     6
+#define TEMPERATURA_EEPROM_ADDRESS      10
 #define FIRST_RUN_EEPROM_ADDRESS        50
 
 // FIRST_RUN_CODE pode ser qualquer valor, preferencialmente diferente de 0x0000 ou 0xFFFF
-// Apenas mude este número para qualquer outro se quiser reiniciar a EEPROM
-#define FIRST_RUN_CODE 187
+// Mude este número para qualquer outro se quiser reiniciar a EEPROM totalmente zerada na próxima compilação
+#define FIRST_RUN_CODE 137
 
 void EEPROMWriteInt(int address, int value);  // 2 Bytes
 int EEPROMReadInt(int address);
