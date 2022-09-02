ac5d84223d1cef70cfc288d76c847e0f9c8d2a96
hvilela0 <hvilela0@gmail.com>
2022-09-02 16:49:46 -0300

    Prepare code to final right temperature sensor

--- lib/TemperatureSensor/TemperatureSensor.cpp
+++ lib/TemperatureSensor/TemperatureSensor.cpp
@@ -1,17 +1,31 @@
 
 #include <TemperatureSensor.h>
-#include <DHT.h>
 
-DHT dht(TEMPERATURE_SENSOR_PIN, DHT11);
+DHT dht(SENSOR_PIN, DHT11);
+
+// Define uma instancia do oneWire para comunicacao com o sensor
+OneWire oneWire(ONE_WIRE_BUS);
+
+// instanciações  para os sensores
+DallasTemperature sensors(&oneWire);
+DeviceAddress sensor1;
 
 void setupTemperatureSensor() {
     dht.begin();
+    sensors.begin();
+    if (!sensors.getAddress(sensor1, 0)) {
+        Serial.println("Sensor de temperatura nao encontrado!");
+    }
 }
 
 //=============================================================================
 
 float getTemperature() {
-    //int tempValue = analogRead(TEMPERATURE_SENSOR_PIN);
     float tempValue = dht.readTemperature();
+
+    sensors.requestTemperatures();
+    // COM O SENSOR FINAL
+    // float tempValue = sensors.getTempC(sensor1);
+
     return tempValue;
 }
--- lib/TemperatureSensor/TemperatureSensor.h
+++ lib/TemperatureSensor/TemperatureSensor.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <Arduino.h>
+#include <DHT.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 
-#define TEMPERATURE_SENSOR_PIN A8
+#define SENSOR_PIN A8
+
+#define ONE_WIRE_BUS SENSOR_PIN
 
 void setupTemperatureSensor();
-
 float getTemperature();
